5a3cdda24ee0d34be69bed291441c816f6571a5d
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2013-01-21 14:31:22 +0300

    Added Devise login as root to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Dashboard::Application.routes.draw do
-  root :to => 'sessions#new'
-  devise_for :users, :path_names => {:sign_in => "login", :sign_out => "logout", :sign_up => "register"}, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  root :to => redirect("/users/login")
+  devise_for :users,
+             :path_names => {:sign_in => "login", :sign_out => "logout", :sign_up => "register"},
+             :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 end
