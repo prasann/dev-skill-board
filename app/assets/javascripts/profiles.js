e5dc926931bda25cb21b8ca117dcd974e22f19af
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2013-02-06 20:59:42 +0300

    Activated nav bar links

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function isProfilePage() {
+    return $('#profile_card').length > 0;
+}
+
+function isWallPage() {
+    return $('#wall_container').length > 0;
+}
+function activate_nav_links() {
+    if (isProfilePage()) {
+        $('#profile_nav_link').addClass('active');
+    }
+    if (isWallPage()) {
+        $('#wall_nav_link').addClass('active');
+    }
+}
+
+$(document).ready(function () {
+    activate_nav_links();
+});
